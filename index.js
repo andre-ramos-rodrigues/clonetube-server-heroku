b8da557ca741e3b355f6a87cff289397cc79269f
Andre <andreramos@id.uff.br>
2022-08-17 13:29:21 -0300

    ..

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use('/api/auth', authRoutes)
 app.use('/api/videos', videosRoutes)
 app.use('/api/comments', commentsRoutes)
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   connect()
-  console.log('Server running at http://localhost:5000')
+  console.log('Server running at port ' + process.env.PORT)
 })
